f300463700f3a0c4ce78cca543ac73bd0fb6df56
isaac <oseasisaac91@gmail.com>
2019-02-09 23:39:17 -0500

    update but does not change the state

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,13 +6,13 @@ class Api::V1::CommentsController < ApplicationController
   end
 
   def update
-    @comment = Comment.find(comment[:id])
+    @comment = Comment.find(params[:id])
     @comment.update(comment_params)
     render json: @comment, :include => [:user, :sighting]
   end
 
   def destroy
-    @comment = Comment.find(comment[:id])
+    @comment = Comment.find(params[:id])
     @comment.delete
     render json: {message: "Successfully deleted"}
   end
