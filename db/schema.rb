eaa06ed02d575a0a09880d1ac784458c92b1210f
github username <github email address>
2019-02-12 13:12:40 -0500

    made changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_07_154925) do
+ActiveRecord::Schema.define(version: 2019_02_12_153859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_02_07_154925) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "lat"
+    t.float "lng"
   end
 
   create_table "users", force: :cascade do |t|
